a739f437dfb35bcca569d8fa73b19466a154cd10
Josef Gullström <josef@pool.fi>
2018-01-23 22:44:52 +0200

    Initialize algorithm instances in middleware.

--- src/store/middleware/geneticMiddleware.js
+++ src/store/middleware/geneticMiddleware.js
@@ -1,9 +1,67 @@
+import genetic from '../../genetic';
+import evolve from '../../genetic/evolve';
+import { bin32Population } from '../../genetic/populations';
+
 import setGlobalLock from '../actions/setGlobalLock';
 import setGeneration from '../actions/setGeneration';
 import updateInstance from '../actions/updateInstance';
 import deleteInstance from '../actions/deleteInstance';
 import setProgress from '../actions/setProgress';
 import clearData from '../actions/clearData';
+
+/**
+ * Compose array of functions from left to right.
+ * @param {Array} funcs 
+ */
+const compose = (funcs) => {
+    if (funcs.length === 0) {
+        return arg => arg;
+    }
+    if (funcs.length === 1) {
+        return funcs[0];
+    }
+    return funcs.reduce((a, b) => (...args) => b(a(...args)));
+}
+
+const composeStage = (bitSize) => (config) => {
+    return compose(config.map(partial => partial.fn(bitSize)(...partial.params)));
+}
+
+const instanceClassFactory = (globalConfiguration) => {
+
+    // Global configuration is common between instances.
+    const fn = genetic.functions[globalConfiguration.fn].fn;
+    const codec = new genetic.codecs.BIN32.class(globalConfiguration.argRanges, null, globalConfiguration.bitSize); // Only binary codec supported atm.
+    const evaluator = genetic.evaluators[globalConfiguration.evaluator];
+    const initializer = genetic.initializers[globalConfiguration.initializer].fn;
+    const populationSize = globalConfiguration.populationSize;
+    const composer = composeStage(globalConfiguration.bitSize);
+    const Population = bin32Population;
+
+    const Instance = class {
+        constructor(instanceConfiguration) {
+            this.population = new bin32Population(populationSize, fn, codec, evaluator, initializer);
+            this.generation = 0;
+
+            const recombine = composer(instanceConfiguration.recombiners);
+            const mutate = composer(instanceConfiguration.mutators);
+            const selectSurvivors = composer(instanceConfiguration.survivorSelectors);
+            const selectParents = composer(instanceConfiguration.parentSelectors);
+
+            const evolver = evolve(selectParents)(recombine)(mutate)(selectSurvivors);
+            this.evolve = () => { this.population = evolver(this.population) };
+        }
+
+        getData() {
+            return this.population;
+        }
+    }
+    return Instance;
+}
+
+// Container for each instance solver.
+let instances = [];
+let Instance;
 
 const geneticMiddleware = store => next => action => {
     if (!action.type.startsWith("GENETIC")) {
@@ -15,6 +73,8 @@ const geneticMiddleware = store => next => action => {
     switch (action.type) {
         // Lock global configuration, initialize solver.
         case "GENETIC_GLOBAL_LOCK":
+            Instance = instanceClassFactory(state.globalConfiguration);
+            console.log(Instance);
             store.dispatch(setGlobalLock(true));
             break;
         // Clear all data.
@@ -23,6 +83,8 @@ const geneticMiddleware = store => next => action => {
             break;
         // Lock instance, evolve to current generation.
         case "GENETIC_INSTANCE_TOGGLE_LOCK":
+            const config = state.instanceConfigurations[action.payload.index];
+            instances.push(new Instance(config));
             store.dispatch(updateInstance({locked: action.payload.locked}, [action.payload.index]))
             break;
         // Clear instance data.
