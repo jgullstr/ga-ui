995158df60bb15ef0ab69de5406807ccab168d6b
Josef Gullström <josef@pool.fi>
2018-02-02 00:37:42 +0200

    Exponential depression mutator

--- src/genetic/mutators.js
+++ src/genetic/mutators.js
@@ -19,12 +19,12 @@ export const mutateRandom = bitSize => p => value => {
 }
 
 /**
- * Inbreeding depression.
+ * Linear inbreeding depression.
  * 
  * @param  {Integer} bitSize Amount of bits in value to mutate.
  * @return {Function} Function that mutates a binary value by flipping bits with probability p.
  */
-export const trickyMutation = bitSize => (min_p, max_p) => function(value) {
+export const linearDepression = bitSize => (min_p, max_p) => function(value) {
     let mask = singleMasks[bitSize - 1];
     let parents = new Uint32Array(this.parents[this.childIndex]);
     const max_parent = Math.max(...parents);
@@ -40,7 +40,29 @@ export const trickyMutation = bitSize => (min_p, max_p) => function(value) {
     return value;
 }
 
+/**
+ * Exponential inbreeding depression.
+ * 
+ * @param  {Integer} bitSize Amount of bits in value to mutate.
+ * @return {Function} Function that mutates a binary value by flipping bits with probability p.
+ */
+export const exponentialDepression = bitSize => (min_p, max_p, exponent) => function(value) {
+    let mask = singleMasks[bitSize - 1];
+    let parents = new Uint32Array(this.parents[this.childIndex]);
+    const max_parent = Math.max(...parents);
+    const min_parent = Math.min(...parents);
+    const max_value = fullMasks[bitSize - 1] >>> 0;
 
+    const distance = (max_parent - min_parent) / max_value;
+    const p = max_p*(1 - Math.pow(distance, exponent)) + min_p;
+    while (mask) {
+        if (randomBoolean(p)) {
+            value ^= mask;
+        }
+        mask >>>= 1;
+    }
+    return value;
+}
 
 const mutators = {
     RANDOM: {
@@ -58,9 +80,9 @@ const mutators = {
         ]
     },
     TRICKY: {
-        name: "Inbreeding depression",
-        description: "Mutation probability decreases relative to the binary difference between parents.",
-        fn: trickyMutation,
+        name: "Inbreeding depression (linear)",
+        description: "Mutation probability decreases linearily relative to the binary difference between parents.",
+        fn: linearDepression,
         params: [
             {
                 name: "Minimal probability",
@@ -75,6 +97,34 @@ const mutators = {
                 type: 'float',
                 range: [0,1],
                 default: 0.01
+            }
+        ]
+    },
+    EXPONENTIAL_DEPRESSION: {
+        name: "Inbreeding depression (exponential)",
+        description: "Mutation probability decreases relative to the binary difference between parents.",
+        fn: exponentialDepression,
+        params: [
+            {
+                name: "Minimal probability",
+                description: "Minimal mutation probability (when parents are vastly different).",
+                type: 'float',
+                range: [0,1],
+                default: 0.01
+            },
+            {
+                name: "Maximal probability",
+                description: "Maximal mutation probability (parents have same value).",
+                type: 'float',
+                range: [0,1],
+                default: 0.01
+            },
+            {
+                name: "Exponent",
+                description: "Inverse rate of exponential descent.",
+                type: 'float',
+                range: [0,10],
+                default: 2
             }
         ]
     }
