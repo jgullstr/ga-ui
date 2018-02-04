fda32edfc18aca7608c2fc2d42b41bd8ba9883c9
Josef Gullström <josef@pool.fi>
2018-02-04 20:41:12 +0200

    Added parent shuffle.

--- src/genetic/parentSelectors.js
+++ src/genetic/parentSelectors.js
@@ -2,7 +2,7 @@ import {randomReal} from './random';
 
 const loadBalancedFitnesses = (population) => {
     let totalFitness = population.totalFitness();
-    let fitnessValues = population.fitnesses();
+    let fitnessValues = population.fitnesses().slice();
     
     const minFitness = Math.min(...fitnessValues);
 
@@ -49,6 +49,18 @@ export const rouletteWheel = bitSize => () => population => {
 };
 
 /**
+ * Shuffle population.
+ * 
+ * Return shuffled population.
+ * 
+ * @param  {Population} population
+ * @return {Population} Same population, with shuffled values.
+ */
+export const shuffle = bitSize => () => population => {
+    return population.shuffle();
+};
+
+/**
  * Stochastic universal sampling.
  * 
  * Create a new, same-size population with members selected at an
@@ -60,11 +72,16 @@ export const rouletteWheel = bitSize => () => population => {
 export const stochasticUniversalSampling = bitSize => () => population => {
 
     const { totalFitness, fitnessValues } = loadBalancedFitnesses(population);
-
     const values = population._values;
 
     const newValues = [];
     const distance = totalFitness / population.size;
+
+    // If population has converged to a single negative fitness, total fitness will be 0.
+    // In that case, the resulting population will be identical to input.
+    if (distance === 0) {
+        return population.fromArray(population._values);
+    }
 
     let target = distance;
     let current = randomReal(0, distance);
@@ -82,6 +99,12 @@ export const stochasticUniversalSampling = bitSize => () => population => {
 
 
 const parentSelectors = {
+    SHUFFLE: {
+        name: "Shuffle",
+        description: "Shuffles the chromosomes in the population. Does no selection and should therefore be used in combination with another selector.",
+        fn: shuffle,
+        params: []
+    },
     ROULETTE_WHEEL: {
         name: "Roulette Wheel Selection",
         description: "Creates a new population with members randomly selected from input based on their fitness.",
